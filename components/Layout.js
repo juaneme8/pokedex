c72e7ddf0c63082e4d4a57bd4f3abc9d53ccc34d
Juan Ocho <juaneme8@gmail.com>
2020-10-16 22:21:25 -0300

    Nextjs and Tailwind working. Layout component rendered

---
+++ components/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Head from 'next/head';
+
+export default function Layout({ title, children }) {
+	return (
+		<div className='bg-red-100'>
+			<Head>
+				<title>{title}</title>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+			<main className='container mx-auto max-w-xl pt-8 min-h-screen'>{children}</main>
+		</div>
+	);
+}
